e811a8b0f0a524568a10c69dcd83b8e07f8bf003
Phil Nova <pnova8@gmail.com>
2017-06-21 17:38:15 -0700

    allow tree to accept nested hash

--- ruby/d2/tree.rb
+++ ruby/d2/tree.rb
@@ -1,9 +1,20 @@
 class Tree
 	attr_accessor :children, :node_name
 
-	def initialize(name, children={})
-		@children = children
-		@node_name = name
+	# problem: modify the tree so it accepts a nested hash for initialization
+
+	# starter code was:
+	# def initialize(name, children=[])
+	# @children = children
+	# @node_name = name
+	#end
+
+	# and would be initialized like so:
+	#ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
+
+	def initialize(h={})
+		@node_name = h.keys.first
+		@children = h.values.map {|x| Tree.new(x)}
 	end
 
 	def visit_all(&block)
@@ -16,7 +27,7 @@ class Tree
 	end
 end
 
-ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
+ruby_tree = Tree.new({'grandpa' => {'dad' => {'sister' => {}, 'brother' => {} }, 'uncle' => {'cousin' => {} }}})
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
@@ -24,3 +35,4 @@ puts
 
 puts "Visiting entire tree"
 ruby_tree.visit_all {|node| puts node.node_name}
+
