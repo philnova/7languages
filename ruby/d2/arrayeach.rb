1e72365126486ea04aca730ba6c2c0b6f93e9fcc
Phil Nova <pnova8@gmail.com>
2017-06-21 17:12:24 -0700

    add explanatory comments

--- ruby/d2/arrayeach.rb
+++ ruby/d2/arrayeach.rb
@@ -1,5 +1,7 @@
+# Problem Prompt: for an array of 16 integers, print slices of four integers
 arr = (1..16).to_a
 
+# first, do this with each
 i = 0
 arr.each do |x|
 	if i % 4 == 0
@@ -8,4 +10,5 @@ arr.each do |x|
 	i += 1
 end
 
+# next, do this with each_slice
 arr.each_slice(4) {|s| puts s.join(',')}
--- ruby/d2/fileread.rb
+++ ruby/d2/fileread.rb
@@ -1,7 +1,33 @@
+filename = "test.txt"
+
+# how to iterate through a file without using blocks
+file = File.new(filename)
+text = file.readlines
+puts text
+file.close
+
+puts
+puts
+
+# how to iterate through a file with blocks
+File.foreach(filename) {|line| puts line}
+
+puts
+puts
+
+# what are the advantages of using blocks?
+# - don't need to explicitly close file
+# - makes it easy to go line by line, rather than slurping whole file into memory
+
+# write a simple grep function that prints all line numbers in a file containing a given phrase
 def grep(filename, phrase)
 	File.foreach(filename).with_index do |line, line_num|
 		puts "#{line_num}: #{line}" if line.include? phrase
 	end
 end
 
+puts "grep for 'which'"
 grep("test.txt", "which")
+
+puts "grep for 'a'"
+grep("test.txt", "a")
